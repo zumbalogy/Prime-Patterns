743c2b58f0aee3fd2b3c1c9327025dc96f5e4334
zumbalogy <intrapostal@gmail.com>
2013-12-21 17:39:40 -0500

    moved to erb

--- god.rb
+++ god.rb
@@ -3,17 +3,5 @@ require 'prime'
 Bundler.require
 
 get '/' do
-    margin = rand(1..100) * 6
-    11000.times.each_with_index.map do |x, i|
-        if Prime.prime?(i)
-            "<style>body {background-color: #222222; padding: 0 #{margin};} div {margin: 0; padding: 0px}</style>
-            <div 
-                style='background-color: cyan; float: left; display: block; border: 1px solid green; height: 5px; width: 5px;'>
-            </div>"
-        else
-            "<div 
-                style='background-color: #222222; float: left; display: block; border: 1px solid black; height: 5px; width: 5px;'>
-            </div>"
-        end
-    end
+
 end
