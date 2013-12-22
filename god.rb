98cd12e3ef0148bc7f789eb4ba3bf633850bdcbc
zumbalogy <intrapostal@gmail.com>
2013-12-22 13:02:03 -0500

    tried some prime caching and all, but no more, and also more divs. sped things up by removing unessarary mapping and each with indexing

--- god.rb
+++ god.rb
@@ -2,6 +2,19 @@ require 'bundler/setup'
 require 'prime'
 Bundler.require
 
+# array = []
+# 14000.times do |x|
+#     if Prime.prime?(x)
+#         array << x
+#     end
+# end
+
+# array = 14000.times.map {|x| x if Prime.prime?(x)}
+# array = Prime.take 2158
+
 get '/' do
+    #@array = array
     erb :index
 end
+
+
